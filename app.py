60bb5dcf616cd0531913bc476a600e119445dccb
christianchueh <100501994+christianchueh@users.noreply.github.com>
2026-05-19 19:05:37 +0800

    Update app.py

--- app.py
+++ app.py
@@ -2,4 +2,4 @@ import streamlit as st
 st.set_page_config(page_title="微型 TimeTree", layout="wide")
 with st.sidebar:
     st.write("###  行事曆群組")
-    st.radio("選擇群組", ["工作", "家庭"])
+    st.radio("選擇群組", ["工作", "家庭" , "朋友"])
