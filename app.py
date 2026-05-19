89e8575429aa33b89829d95679fd20447cb63659
christianchueh <100501994+christianchueh@users.noreply.github.com>
2026-05-19 20:51:34 +0800

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,8 @@ import datetime
 st.set_page_config(page_title="微型 TimeTree", layout="wide")
 mode = st.radio("選擇群組" , ["學生" , "老師" , "家長會" , "校友會"],horizontal=True)
 
-st.session_state.mylist = []
+if mylist not in st.session_state:
+    st.session_state.mylist = []
 l , r = st.columns(2)
 
 with l:
